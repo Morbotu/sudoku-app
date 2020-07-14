d17e7160f59823d843aa3d20f2e09b31c25b1ad4
Rombout Jansen <romboutlambert@gmail.com>
2020-07-14 14:06:58 +0200

    Split the data file to make it faster

--- createSudokus/split_file.py
+++ createSudokus/split_file.py
@@ -4,9 +4,10 @@ with open("sudoku.csv", "r") as f:
     content = f.read()
 
 content = content.split("\n")
-n = ceil(1000000/7)
+numberOfSplitFile = int(input("How many files do you want?\n"))
+n = ceil(1000000/numberOfSplitFile)
 splitContent = [content[i:i+n] for i in range(0, len(content), n)]
 
-for i in range(len(splitContent)):
+for i in range(len(splitContent)-1):
     with open("sudokuDataFile" + str(i+1) + ".csv", "w") as f:
         f.write("\n".join(splitContent[i]))
